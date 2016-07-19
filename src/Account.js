35d571106fb63179ef6cfffe1a78acf5c1e062c0
Jim Barrows <jim@reallybigtree.com>
2016-07-18 18:38:51 -0700

    Debugging

--- src/Account.js
+++ src/Account.js
@@ -1,10 +1,13 @@
+'use strict';
 const mongoose              = require('mongoose'),
       Schema                = mongoose.Schema,
+      Types                 = Schema.Types,
       passportLocalMongoose = require('passport-local-mongoose');
 
+console.log("Account schema");
 var Account = new Schema({
-	username: String,
-	password: String
+	username: Types.String,
+	password: Types.String
 });
 
 Account.plugin(passportLocalMongoose);

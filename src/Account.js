106cf6f86a4947f428d5162709cbb095176fc7fc
Jim Barrows <jim@reallybigtree.com>
2016-07-20 15:40:42 -0700

    Removed extraneous comma

--- src/Account.js
+++ src/Account.js
@@ -5,7 +5,7 @@ const mongoose              = require('mongoose'),
       passportLocalMongoose = require('passport-local-mongoose');
 
 var Account = new Schema({
-	facebookUserId: Types.String,
+	facebookUserId: Types.String
 });
 
 Account.plugin(passportLocalMongoose);

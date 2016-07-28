47a3e5df59525b7592df0f021d602c46a6837254
Jim Barrows <jim@reallybigtree.com>
2016-07-28 08:56:09 -0700

    Added twitter account information

--- src/Account.js
+++ src/Account.js
@@ -4,10 +4,18 @@ const mongoose              = require('mongoose'),
       Types                 = Schema.Types,
       passportLocalMongoose = require('passport-local-mongoose');
 
+var TwitterAccount = new Schema({
+	ownerId: Types.String,
+	owner: Types.String,
+	accessToken: Types.String,
+	accessTokenSecret: Types.String
+});
+
 var Account = new Schema({
 	facebookUserId: Types.String,
 	accessToken: Types.String,
-	expiresIn: Types.Date
+	expiresIn: Types.Date,
+	twitterAccounts: [TwitterAccount]
 });
 
 Account.plugin(passportLocalMongoose);

afaffad45b54fc8fcd453ab09ffd0de2b1e74082
Jim Barrows <jim@reallybigtree.com>
2016-08-17 13:14:33 -0700

    Setting up to make it easier to use in the 5/6 mix environment.

---
+++ src/index.js
@@ -0,0 +1,25 @@
+/**
+ * Created by JimBarrows on 7/18/16.
+ */
+'use strict';
+import Account from "./Account";
+import Asset from "./Asset";
+import Content from "./Content";
+import Destination from "./Destination";
+import FacebookAccountInfo from "./FacebookAccountInfo";
+import Keyword from "./Keyword";
+import Message from "./Message";
+import TwitterAccountInfo from "./TwitterAccountInfo";
+import WordPressAccountInfo from "./WordPressAccountInfo";
+module.exports = {
+		Account,
+		Asset,
+		Content,
+		Destination,
+		FacebookAccountInfo,
+		Keyword,
+		Message,
+		TwitterAccountInfo,
+		WordPressAccountInfo
+};
+

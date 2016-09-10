53b6f19fc27d02f771aff5b10de2a2b7a04508ba
Jim Barrows <jim@reallybigtree.com>
2016-09-09 17:24:32 -0700

    Updating facebook to handle multiple pages.

--- src/FacebookAccountInfo.js
+++ src/FacebookAccountInfo.js
@@ -3,10 +3,18 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema,
       Types  = mongoose.Schema.Types;
 
-const FacebookAccountInfo = new Schema({
+const PageInfo = new Schema({
+	accessToken: Types.String,
+	category: Types.String,
 	name: Types.String,
-	pageId: Types.String,
-	accessToken: Types.String
+	id: Types.String
+});
+
+const FacebookAccountInfo = new Schema({
+	accessToken: Types.String,
+	name: Types.String,
+	pages: [PageInfo]
+
 });
 
 export default FacebookAccountInfo;
